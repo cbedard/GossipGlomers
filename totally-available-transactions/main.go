f6b2f182c69e3c637bbbe1a111207b9724704f22
Cameron Bedard <cam.bedardca@gmail.com>
2025-04-16 18:54:30 -0400

    challenge 6b - multi node read-uncommitted

--- totally-available-transactions/main.go
+++ totally-available-transactions/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 	"log"
 	"os"
 	"sync"
+	"time"
 )
 
 var db map[int]int
 var mu *sync.Mutex
+
+// GO ROUTINES ARE FIFO
+// db.go where we accept transactions on a channel
+// how do we want to sync outbound?
 
 func main() {
 	n := maelstrom.NewNode()
@@ -18,32 +24,61 @@ func main() {
 	mu = &sync.Mutex{}
 
 	n.Handle("txn", func(msg maelstrom.Message) error {
-		body := getBody(msg)
-		txn := body["txn"].([]any)
-
 		mu.Lock()
 		defer mu.Unlock()
 
-		for i := range txn {
-			transaction := txn[i].([]any)
-			operation := transaction[0].(string)
-			key := int(transaction[1].(float64))
+		body := getBody(msg)
+		txn := body["txn"].([]any)
 
-			if operation == "r" {
-				//handle read
-				val, ok := db[key]
-
-				if ok {
-					transaction[2] = val
-				}
-			} else {
-				//handle write
-				val := int(transaction[2].(float64))
-				db[key] = val
+		handleTransaction(txn)
+		//sync all nodes
+		for _, neighbour := range n.NodeIDs() {
+			if neighbour == n.ID() {
+				continue
 			}
+
+			//need a new body (copy) to avoid memory overwriting from other threads
+			syncBody := map[string]any{"type": "sync", "txn": txn}
+
+			go func() {
+				for i := range 10 {
+					ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+
+					_, err := n.SyncRPC(ctx, neighbour, syncBody)
+
+					if err == nil {
+						cancel()
+						break
+					} else {
+						if i > 6 {
+							appendToLogFile(struct {
+								Retry int
+								Err   string
+							}{
+								Retry: i,
+								Err:   err.Error(),
+							})
+						}
+					}
+					cancel()
+				}
+			}()
 		}
 
 		body["type"] = "txn_ok"
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("sync", func(msg maelstrom.Message) error {
+		mu.Lock()
+		defer mu.Unlock()
+
+		body := getBody(msg)
+		txn := body["txn"].([]any)
+
+		handleTransaction(txn)
+
+		body["type"] = "sync_ok"
 		return n.Reply(msg, body)
 	})
 
@@ -52,8 +87,28 @@ func main() {
 	}
 }
 
-// -------  UTILS  -------
+func handleTransaction(txn []any) {
+	for i := range txn {
+		transaction := txn[i].([]any)
+		operation := transaction[0].(string)
+		key := int(transaction[1].(float64))
 
+		if operation == "r" {
+			//handle read
+			val, ok := db[key]
+
+			if ok {
+				transaction[2] = val
+			}
+		} else {
+			//handle write
+			val := int(transaction[2].(float64))
+			db[key] = val
+		}
+	}
+}
+
+// -------  UTILS  -------
 //txn entry ["r", 1 (key), null] OR ["w", 1 (key), 3 (val)]
 
 func getBody(msg maelstrom.Message) map[string]any {
